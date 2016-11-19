f83accf4ff3d6bf5a92486bf14a99ce5f399765a
William Vuong <wnvuong@vt.edu>
2016-11-18 21:46:50 -0500

    refactored movement log to come from app.js

--- src/App.js
+++ src/App.js
@@ -4,22 +4,39 @@ import AppContent from './AppContent.js';
 import DailyLog from './DailyLog.js';
 import AddMovementModal from './AddMovementModal.js';
 import { Layout, Header, HeaderRow, Content } from 'react-mdl';
+import apihelper from './utils/apihelper.js';
 
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      date: new Date(2016, 10, 11),
+      movementLog: { movements: [] }
+    };
+
+    apihelper.getMovementLogs(this.state.date).then(movementLogs => {
+      this.setState({
+        movementLog: movementLogs[0]
+      });
+    });
+  }
   render() {
-    const date = new Date(2016, 10, 11);
     return (
       <div>
         <Layout fixedHeader className='mdl-color--grey-50'>
           <Header>
-            <HeaderRow className='content-container app-header' title={<span><strong>{date.toLocaleDateString()}</strong></span>} />
+            <HeaderRow className='content-container app-header' 
+            title={<span><strong>{this.state.date.toLocaleDateString()}</strong></span>} />
           </Header>
           <Content>
-            <DailyLog className='content-container' date={date}/>
-            <AddMovementModal date={date} />
+            <DailyLog className='content-container' 
+            date={this.state.date}
+            movementLog={this.state.movementLog} />
+            <AddMovementModal date={this.state.date} />
           </Content>
         </Layout>
       </div>
--- src/DailyLog.js
+++ src/DailyLog.js
@@ -4,34 +4,19 @@ import apihelper from './utils/apihelper.js';
 import datehelper from './utils/datehelper.js';
 
 class DailyLog extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      movementLogsView: []
-    };
-
-    apihelper.getMovementLogs(this.props.date).then(movementLogs => {
-      console.log(movementLogs)
-      const movementLogsView = movementLogs[0].movements.map((movement, index) => {
-        return (
-          <MovementLog
-            key={index}
-            sets={movement.sets}
-            name={movement.movement.name}
-            />
-        )
-      });
-
-      this.setState({
-        movementLogsView: movementLogsView
-      });
-    });
-  }
   render() {
+    const movementLog = this.props.movementLog.movements.map((movement, index) => {
+      return (
+        <MovementLog
+          key={index}
+          sets={movement.sets}
+          name={movement.movement.name}
+          />
+      )
+    });
     return (
       <div className={this.props.className + ' daily-log'}>
-        {this.state.movementLogsView}
+        {movementLog}
       </div>
     )
   }
