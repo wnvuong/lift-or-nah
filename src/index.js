c9274e017b66a8ac3df2d6f339d538203a6cf19b
William Vuong <wnvuong@vt.edu>
2016-11-20 19:22:41 -0500

    removed broken react-mdl references

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'react-mdl/extra/material.css';
-import 'react-mdl/extra/material.js';
 import './stylesheets/main.scss';
 
 ReactDOM.render(
